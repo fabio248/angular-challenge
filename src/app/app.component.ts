c0e4be01213208a197efc52e5266e7f22d809af0
Fabio Flores <fabioflores021@gmail.com>
2024-06-14 11:36:41 -0600

    feat: add angular material theme and tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatMiniFabButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggle, MatMiniFabButton, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-challenge';
