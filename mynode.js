73d952665ac3a1c8f704239a8d2dc58bb3569633
fabioflores <fabioflores021@gmail.com>
2024-08-14 06:41:11 -0600

    feat: add config env variables vercel

---
+++ mynode.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+
+const envFile = `export const environment = {
+  env: process.env['ENV'],
+  apiToken: process.env['API_TOKEN'],
+  apiUrl: process.env['API_URL'],
+};
+`;
+const targetPath = path.join(__dirname, './src/environments/environment.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+  }
+});
