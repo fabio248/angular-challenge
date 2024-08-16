270a57180560f00a7d4d7014a5dbdbf6bd23f506
fabioflores <fabioflores021@gmail.com>
2024-08-16 08:13:38 -0600

    feat: add initial structure my task

---
+++ src/app/features/my-task/components/table-header/table-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-header.component.html',
+  styleUrl: './table-header.component.scss',
+})
+export class TableHeaderComponent {}
--- src/app/features/my-task/my-task.component.ts
+++ src/app/features/my-task/my-task.component.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable,
+} from '@angular/material/table';
+import { Observable } from 'rxjs';
+import { TaskModel } from '../../core/models/task.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../core/state/app.state';
+import { loadTasks } from '../../core/state/actions/task.action';
+import { selectListTasks } from '../../core/state/selectors/task.selector';
+import { TimeTaskComponent } from '../dashboard/components/time-task/time-task.component';
+import { TagListComponent } from '../dashboard/components/tag-list/tag-list.component';
+import { TableHeaderComponent } from './components/table-header/table-header.component';
 
 @Component({
   selector: 'app-my-task',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatRowDef,
+    MatHeaderRowDef,
+    TimeTaskComponent,
+    TagListComponent,
+    TableHeaderComponent,
+  ],
   templateUrl: './my-task.component.html',
+  styleUrl: './my-task.component.scss',
 })
-export class MyTaskComponent {}
+export class MyTaskComponent implements OnInit {
+  readonly store: Store<AppState> = inject(Store);
+  displayedColumns: string[] = ['name', 'tags', 'pointEstimate', 'dueDate'];
+  task: Observable<TaskModel[]> = new Observable();
+  dataSource: TaskModel[] = [];
+  myTask: TaskModel[] = [];
+
+  ngOnInit(): void {
+    this.store.dispatch(loadTasks());
+    this.task = this.store.select(selectListTasks);
+
+    this.task.subscribe((tasks) => {
+      this.myTask = tasks.filter(
+        (task) => task.assignee.fullName === 'Fabio Flores',
+      );
+
+      this.dataSource = this.myTask;
+      console.log(this.myTask);
+    });
+  }
+}
